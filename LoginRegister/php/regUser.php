f519639e8929bcd2e94e414f23ddd9a9c70a2bac
Csengori <sagi.tamas@hotmail.com>
2015-11-04 00:27:01 +0000

    reg test 2

--- LoginRegister/php/regUser.php
+++ LoginRegister/php/regUser.php
@@ -8,7 +8,14 @@ require('..\connections\connect.php');
 
 /* check is form is submited */
 	$succes = false;
-	if (isset($_POST['regBTN']) && (isset($_POST['username'])) && (isset($_POST['email'])) && (isset($_POST['password'])) ) {
+	if (isset($_POST['regBTN'])			&& 
+		(isset($_POST['username'])) 	&&  (!empty($_POST['username']))
+		 && (isset($_POST['email']))	&&	(!empty($_POST['email']))
+		 && (isset($_POST['password']))	&&  (!empty($_POST['password']))) 
+	{
+
+
+
 		session_start();
 		$username	= $_POST['username'];
 		$email 		= $_POST['email'];
@@ -19,13 +26,18 @@ require('..\connections\connect.php');
 	$sql = $conn -> query("INSERT INTO users (username, email ,password)Values ('{$username}', '$email', '$pass')");
 
 
-	}
-	
-
-
-	
-
 	header('Location: ..\index.html');
+	}
+
+	
+
+	
+	else echo "FuckYou";
+
+
+	
+
+	
 
 
 
