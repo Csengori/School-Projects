2cf42947b02122ee127e8651d9cc5fd597726bb6
Csengori <sagi.tamas@hotmail.com>
2015-11-04 01:50:31 +0000

    todo added

--- LoginRegister/php/loginUser.php
+++ LoginRegister/php/loginUser.php
@@ -2,12 +2,16 @@
 require('..\connections\connect.php');
 
 
+if(isset($_POST))
+
+ $username = $_POST['username'];
+ $password = $_POST['username'];
+
+ $sql = $conn 	->query("select * from users where username='$username' AND password='$password'");
+ $row = $sql 	->fetch(MYSQLI_BOTH);
 
 
-echo "hello";
-
-
-
+session_start();
 
 
 
